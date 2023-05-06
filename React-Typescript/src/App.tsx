95682ce1684d1f571ed9952ebfc93d524c27a3ce
Laura Luna <lauraluna.siqueira@hotmail.com>
2023-05-06 16:49:24 -0300

    Create React-Typescript

---
+++ React-Typescript/src/App.tsx
@@ -0,0 +1,88 @@
+import React, {createContext} from 'react';
+
+import FirstComponnet  from './components/FirstComponnet';
+
+import SecondComponent from './components/SecondComponent';
+
+import Destructuring, {Category} from './components/Destructuring';
+
+import State from './components/State';
+
+import Context from './components/context';
+
+
+type textOrNull = string | null
+
+interface IAppContext {
+  language: string,
+  framework: string,
+  projects: number
+}
+
+export const AppContext = createContext<IAppContext | null>(null)
+
+
+type fixed = "Isso" | "Ou" | "Aquilo"
+
+
+function App() {
+
+  const name: string = "Laura";
+  const age: number = 30;
+  const isWorking: boolean = false;
+
+  const userGreeting = (name: string): string => {
+    return `Olá. ${name}`
+  }
+
+  const myText:textOrNull = "Tem algum texto aqui!"
+  const mySecondText:textOrNull = null
+
+  const opa:fixed = "Ou"
+
+  const contextValue = {
+    language: "JavaScript",
+    framework: "Express",
+    projects: 5
+  }
+
+  return (
+    <AppContext.Provider value={contextValue}>
+    <div className="App">
+      <h1>
+        Open 
+      </h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && (
+        <div>
+          <p>Está trabalhando</p>
+        </div>
+      )}
+      <h3>{userGreeting(name)}</h3>
+
+      <FirstComponnet />
+      <SecondComponent name="segundo"/>
+      <Destructuring 
+          title="Primeiro post" 
+          content="Post muiiiito bom, sobre TS" 
+          commentsQty={10} tags={["JavaScript", "TypeScript"]} 
+          category={Category.TS} 
+        />
+        <State />
+        {myText && 
+          <p>Tem texto em myText</p>
+        }
+        {mySecondText &&
+          <p>Tem texto em mySecondText</p>
+        }
+        {(!myText || !mySecondText) &&
+          <p>Alguma das variáveis de texto está vazia!</p>
+        }
+    </div>
+    <Context />
+    </AppContext.Provider>
+  );
+ }
+
+export default App;
---
+++ React-Typescript/src/components/FirstComponnet.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react'
+
+
+function FirstComponnet(): ReactElement {
+  return (
+   <div>
+    <h1>Meu primeiro component</h1>
+   </div>
+  );
+}
+
+export default FirstComponnet;
