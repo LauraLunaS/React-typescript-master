4d00430430413fb7725214560dde9796d4778409
Laura Luna <lauraluna.siqueira@hotmail.com>
2023-05-06 23:17:38 -0300

    header e footer

---
+++ projeto/src/interfaces/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './App.module.css'
+
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main className={styles.main}>
+        <h1>Conteudo</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
