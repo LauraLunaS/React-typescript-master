efcd7b4f08a0d4f4448d04ef3e911b2b8e5da484
Laura Luna <lauraluna.siqueira@hotmail.com>
2023-05-10 16:36:25 -0300

    Api Restfull

---
+++ Api RESTfull/src/app.ts
@@ -0,0 +1,18 @@
+import express from "express"
+import config from "config"
+
+const app = express()
+
+app.use(express.json())
+
+
+import router from './router'
+
+app.use("/api/", router);
+
+const port = config.get<number>("port");
+
+app.listen(port, async () => {
+console.log(`Aplicação está funcionando na porta: ${port}`);
+});
+
