c24a97dbbf0d36d21c59933d82bbb280a71b3d07
Laura Luna <lauraluna.siqueira@hotmail.com>
2023-05-12 19:01:43 -0300

    model e controllers

---
+++ Api RESTfull/src/controllers/movieControllers.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express"
+
+import { MovieModel } from "../models/model"
+
+import Logger from "../../config/logger"
+
+
+export async function createMovie(req: Request, res: Response) {
+    return res.status(200).send("Deu certo o controller")
+}
